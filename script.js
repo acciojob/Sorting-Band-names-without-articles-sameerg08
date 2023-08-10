2977a46d73a7fa610ab6970d501d4f0d52336bb3
Sameer Gupta <128709790+sameerg08@users.noreply.github.com>
2023-08-10 16:47:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-//your code here
-
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+let updatedList=touristSpots.map((element)=>{
+	let words=element.split(" ")
+	let updatedString=words.reduce((prev,curr,index)=>{
+		if(curr!="The" && curr!="an" && curr!="a"){
+			return prev+" "+curr;
+		}
+		return prev;
+	},"")
+	return updatedString;
+});
+let mp={};
+updatedList.forEach((element,index)=>{
+	mp[element]=arr[index];
+});
+updatedList.sort();
+let finalAns=updatedList.map((element)=>{
+	return mp[element];
+})
+console.log(finalAns);
